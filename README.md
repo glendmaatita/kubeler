55d77d34d8ca2f95da4f67f72bd5a90ca2915cc3
Glend Maatita <glend.maatita@gmail.com>
2025-02-15 17:35:04 +0700

    add installer

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kubeler
+
+Simple Kubernetes Resources installer
+
+Dependencies
+- Python >= 3.12
+
+# Installation
+
+```
+pip install kubeler
+```
+
+## Usage
+```
+kubeler install --installer=./examples/installer.yaml
+```
---
+++ kubeler/main.py
@@ -0,0 +1,20 @@
+import argparse
+from scripts.installer import Installer
+
+def main():
+    parser = argparse.ArgumentParser(description="Installer script")
+    subparsers = parser.add_subparsers(dest='command', required=True, help='Subcommands')
+
+    # Create a subparser for the 'install' command
+    install_parser = subparsers.add_parser('install', help='Install command')
+    install_parser.add_argument('--installer', type=str, default='./installer.yaml', help='Path to the config YAML file')
+    install_parser.add_argument('--kube-config', type=str, default='~/kube/config', help='Path to the kube config file')
+
+    args = parser.parse_args()
+
+    if args.command == 'install':
+        installer = Installer(installer=args.installer, kube_config=args.kube_config)
+        installer.install()
+
+if __name__ == "__main__":
+    main()
